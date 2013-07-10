0043488bb7cdb216f04f01bd9117970a75f29796
Christian Mohn <christian@drible.net>
2013-07-11 00:03:11 +0200

    Added bash_profile and simple symlink creation script

---
+++ .bash_profile
@@ -0,0 +1,5 @@
+source ~/.bashrc
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+alias httpd='python -m SimpleHTTPServer'
+alias publicip='curl ifconfig.me/ip'
---
+++ symlinks.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+############################
+# .make.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Heavily inspired/stolen from MICHAEL SMALLEY http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
+# https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+############################
+
+
+########## Go Home
+cd ~
+
+
+########## Create Symlinks
+ln dotfiles/.bash_profile .bash_profile
+ln dotfiles/.slate .slate
